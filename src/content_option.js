c36806da99530a6b1d265e55bf6cfcfa9f40faa1
Ayon Jodder <ayonjodder177@gmail.com>
2022-09-10 12:37:43 +0600

    All done

---
+++ src/content_option.js
@@ -0,0 +1,167 @@
+const logotext = "AYON";
+const meta = {
+    title: "Ayon Jodder",
+    description: "I’m Ayon Jodder _ MERN Stack Developer, currently working at Eddokta",
+};
+
+const introdata = {
+    title: "I’m Ayon Jodder",
+    animated: {
+        first: "I'm a MERN Stack Developer",
+        second: "I'm a problem solver",
+        third: "I'm a self learner",
+    },
+    description: "I'm a dedicated and passionate MERN Developer. I'm a quick learner and eager to learn new technologies to expand my knowledge. Currently, I’m focused on building responsive full-stack web applications.",
+    your_img_url: "https://i.ibb.co/yNnnZY8/Completed.jpg",
+};
+
+const dataabout = {
+    title: "A bit about myself",
+    aboutme: "I'm a student of Green University of Bangladesh in BSc in CSE. I have completed the MERN Stack Development Course from Programming Hero. Currently working as a React Developer at Eddokta. Completed an internship at iSchool as Web Developer. Also leaded a team of 5 members in a project named 'Explorer' as a Team Leader. I have gained a bunch of experience in leadership, teamwork, and problem-solving there. Long way to go, many technologies to learn, and many projects to build.",
+};
+const worktimeline = [{
+    jobtitle: "Designer of week",
+    where: "YAdfi",
+    date: "2020",
+},
+{
+    jobtitle: "Designer of week",
+    where: "Jamalya",
+    date: "2019",
+},
+{
+    jobtitle: "Designer of week",
+    where: "ALquds",
+    date: "2019",
+},
+];
+
+const skills = [{
+    name: "JavaScript",
+    value: 85,
+},
+{
+    name: "React",
+    value: 90,
+},
+{
+    name: "Node",
+    value: 75,
+},
+{
+    name: "Exxpress",
+    value: 75,
+},
+{
+    name: "MongoDB",
+    value: 80,
+},
+];
+
+const services = [{
+    title: "Web Application Development",
+    description: "I have developed more than 10 web applications using MERN Stack and 15+ web application using JavaScript.",
+},
+{
+    title: "Backend Development",
+    description: "I love backend development most. I have developed more than 10 backend applications using Node, Express, and MongoDB. Have made tons of API for CRUD operations using Node, Express and MongoDB.",
+},
+{
+    title: "Wordpress Design",
+    description: "Worked as a WordPress Developer for an year in Fiverr. I'm good at Elementor, Divi, WP Bakery, TagDiv, and many more. Also I have completed a WordPress speed optimization course from Hasin Hayder Sir.",
+},
+];
+
+const dataportfolio = [{
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/800/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/600/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/300/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/700/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+
+{
+    img: "https://picsum.photos/400/600/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/300/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/550/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/700/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "ayonjodder177@domain.com",
+    YOUR_FONE: "+880 192 020 4818",
+    description: "Submit the form below or shoot me an email - ayonjodder177@gmail.com",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_hcdbx26",
+    YOUR_TEMPLATE_ID: "template_6j33729",
+    // YOUR_USER_ID: "Ayon Jodder",
+    YOUR_PUBLIC_KEY: "5HauwUB4KAZ8a2BNH",
+};
+
+const socialprofils = {
+    github: "https://github.com/AyonJD",
+    facebook: "https://www.facebook.com/ayon.jodder.75/",
+    linkedin: "https://www.linkedin.com/in/ayon-jodder/",
+    gmail: "mailto:ayonjodder177@gmail.com"
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
